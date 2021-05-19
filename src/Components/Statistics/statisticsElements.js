d37e6d336974d7d3ea947e0e6d91c2201928fbae
meryam <miracoyamamoto@gmail.com>
2021-05-19 17:00:39 +0100

    working on home page components

---
+++ src/Components/Statistics/statisticsElements.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StatisticContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ColoredStatistic = styled.div``;
